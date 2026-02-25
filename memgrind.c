f8b4b30320b46a9c3d7f503d9a9592ed7f008265
Sarthak <sr.sarthak.talukdar@gmail.com>
2026-02-25 17:18:55 -0500

    Added the malloc include file

--- memgrind.c
+++ memgrind.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <sys/time.h> 
 
+// Compile with -DREALMALLOC to use the real malloc() instead of mymalloc()
+#ifndef REALMALLOC
+#include "mymalloc.h"
+#endif
+
 int main(int argc, char* argv[]){
 
 
